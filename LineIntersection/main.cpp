9afe499f7c91363d79ae81883ab28006ec3a8ae5
Roshan Gautam <rgautam@astate.edu>
2015-06-01 17:52:18 -0500

    Add Source File

--- LineIntersection/main.cpp
+++ LineIntersection/main.cpp
@@ -7,9 +7,15 @@
 //
 
 #include <iostream>
+using namespace std;
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int x1 = 0, y1 = 0;
+    cout << "Enter Coordinates for first point!\n";
+    cout << "Enter x1:";
+    cin >> x1;
+    cout << "Enter y1:";
+    cin >> y1;
+    cout << "x1:" <<  x1 << ", y1:" << y1;
     return 0;
 }
