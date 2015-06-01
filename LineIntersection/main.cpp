b7678b3f4ec69cf941c1f9b3e5cfa4a9b10feee6
Roshan Gautam <rgautam@astate.edu>
2015-06-01 17:35:03 -0500

    Initial Commit

---
+++ LineIntersection/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LineIntersection
+//
+//  Created by Roshan Gautam on 6/1/15.
+//  Copyright (c) 2015 Roshan Gautam. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
