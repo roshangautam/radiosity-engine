94d92fe4e51cda50c21c69bc0cb79d377d55b149
Roshan Gautam <rgautam@astate.edu>
2015-06-08 20:17:59 -0500

    Code cleanup

--- RadiosityEngine/3dintersection.h
+++ RadiosityEngine/3dintersection.h
@@ -131,19 +131,6 @@ public:
             cout << " on " << getHumanReadableIntersectingFace() << "\n\n";
         }
     }
-    
-    
-//    function SameSide(p1,p2, a,b)
-//    cp1 = CrossProduct(b-a, p1-a)
-//    cp2 = CrossProduct(b-a, p2-a)
-//    if DotProduct(cp1, cp2) >= 0 then return true
-//        else return false
-//            
-//    function PointInTriangle(p, a,b,c)
-//    if SameSide(p,a, b,c) and SameSide(p,b, a,c)
-//        and SameSide(p,c, a,b) then return true
-//        else return false
-    
 };
 
 #endif
